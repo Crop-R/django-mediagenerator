ad7a319b36654ff59f2b2ff169cdb59409809ab7
blankser <erikjan.blanksma@dacom.nl>
2017-08-02 15:33:57 +0200

    fix django 1.10 compatibility

--- mediagenerator/management/commands/generatemedia.py
+++ mediagenerator/management/commands/generatemedia.py
@@ -6,6 +6,6 @@ class Command(BaseCommand):
 
     requires_model_validation = False
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         prepare_media()
         generate_media()
